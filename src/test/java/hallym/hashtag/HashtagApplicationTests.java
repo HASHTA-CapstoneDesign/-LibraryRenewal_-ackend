4d71527a5b33da9df2d05bd32310681d57ffef5c
mijin0721 <kgmi2010@naver.com>
2023-03-19 16:37:30 +0900

    first commit

---
+++ src/test/java/hallym/hashtag/HashtagApplicationTests.java
@@ -0,0 +1,13 @@
+package hallym.hashtag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashtagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
