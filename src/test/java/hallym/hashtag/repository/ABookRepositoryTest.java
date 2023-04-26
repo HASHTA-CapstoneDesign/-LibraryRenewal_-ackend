10ba283f904457201170e596101c3e69baac5a88
mijin0721 <kgmi2010@naver.com>
2023-04-26 11:22:03 +0900

    test : ABookRepository에 save부분 테스트

---
+++ src/test/java/hallym/hashtag/repository/ABookRepositoryTest.java
@@ -0,0 +1,29 @@
+package hallym.hashtag.repository;
+
+import hallym.hashtag.domain.ABook.entity.ABook;
+import hallym.hashtag.domain.ABook.repostory.ABookRepository;
+import hallym.hashtag.domain.book.entity.Book;
+import hallym.hashtag.domain.book.repository.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+public class ABookRepositoryTest {
+    @Autowired
+    ABookRepository aBookRepository;
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    public void testSave() {
+        Optional<Book> byBno = bookRepository.findById(1L);
+        ABook aBook = ABook.builder()
+                .tag("gg")
+                .book(byBno.get())
+                .build();
+        aBookRepository.save(aBook);
+    }
+}
