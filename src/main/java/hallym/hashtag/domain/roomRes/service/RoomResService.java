ce4cc42ce1810cc533d62be3f658d9226b971812
mijin0721 <kgmi2010@naver.com>
2023-04-29 04:07:31 +0900

    feat : list를 페이징 처리하기 위한 dto 생성

--- src/main/java/hallym/hashtag/domain/roomRes/service/RoomResService.java
+++ src/main/java/hallym/hashtag/domain/roomRes/service/RoomResService.java
@@ -1,9 +1,8 @@
 package hallym.hashtag.domain.roomRes.service;
 
-
 import hallym.hashtag.domain.roomRes.dto.RoomResResponseDto;
 import hallym.hashtag.domain.roomRes.entity.Time;
 
 public interface RoomResService {
-
+    RoomResResponseDto reserve(Long sno, String roomName, Time time);
 }
--- src/main/java/hallym/hashtag/domain/roomRes/service/RoomResServicelmpl.java
+++ src/main/java/hallym/hashtag/domain/roomRes/service/RoomResServicelmpl.java
@@ -1,5 +1,8 @@
 package hallym.hashtag.domain.roomRes.service;
 
+import hallym.hashtag.domain.roomRes.dto.RoomResResponseDto;
+import hallym.hashtag.domain.roomRes.entity.RoomRes;
+import hallym.hashtag.domain.roomRes.entity.Time;
 import hallym.hashtag.domain.roomRes.repository.RoomResRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,4 +14,22 @@ import javax.transaction.Transactional;
 @Service
 public class RoomResServicelmpl implements RoomResService {
     private final RoomResRepository roomResRepository;
+
+    @Override
+    public RoomResResponseDto reserve(Long sno, String roomName, Time time) {
+
+        RoomRes roomRes = RoomRes.builder().build();
+        return null;
+    }
+
+    public RoomResResponseDto toDto(RoomRes roomRes) {
+        return RoomResResponseDto.builder()
+                .rno(roomRes.getRno())
+                .creDate(roomRes.getCreDate())
+                .useDate(roomRes.getUseDate())
+                .reserve(roomRes.isReserve())
+                .roomName(roomRes.getRoomName())
+                .time(roomRes.getTime())
+                .build();
+    }
 }
