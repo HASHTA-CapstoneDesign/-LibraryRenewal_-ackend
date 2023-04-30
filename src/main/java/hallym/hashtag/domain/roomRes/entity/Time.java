7b7685966f7df137b950d02ede0ea34b59312b56
mijin0721 <kgmi2010@naver.com>
2023-04-30 17:55:13 +0900

    delete : roomRes 파일 삭제

--- src/main/java/hallym/hashtag/domain/roomRes/entity/Time.java
+++
@@ -1,5 +0,0 @@
-package hallym.hashtag.domain.roomRes.entity;
-
-public enum Time {
-    오전9시, 오전10시, 오전11시, 오전12시, 오후1시, 오후2시, 오후3시, 오후4시, 오후5시, 오후6시, 오후7시, 오후8시, 오후9시
-}
--- src/main/java/hallym/hashtag/domain/roomRes/service/RoomResService.java
+++
@@ -1,9 +0,0 @@
-package hallym.hashtag.domain.roomRes.service;
-
-import hallym.hashtag.domain.roomRes.dto.RoomResRequestDto;
-import hallym.hashtag.domain.roomRes.dto.RoomResResponseDto;
-import hallym.hashtag.domain.roomRes.entity.Time;
-
-public interface RoomResService {
-    RoomResResponseDto reserve(Long sno, Long rno, RoomResRequestDto roomResRequestDto);
-}
