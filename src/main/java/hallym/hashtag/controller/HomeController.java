5a48140d5e78996c5abd9cde0c8003437829349c
mijin0721 <kgmi2010@naver.com>
2023-05-07 01:06:21 +0900

    build : swagger 의존성 추가

--- src/main/java/hallym/hashtag/controller/HomeController.java
+++ src/main/java/hallym/hashtag/controller/HomeController.java
@@ -1,5 +1,8 @@
 package hallym.hashtag.controller;
 
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/home")
 public class HomeController {
+
     @GetMapping
     public String getHome() {
         return "test";
