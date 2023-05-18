8868c94794b158f85a132fed032c3d8714d736b4
mijin0721 <kgmi2010@naver.com>
2023-05-18 13:35:58 +0900

    delete : user 회원가입 기능 삭제

--- src/main/java/hallym/hashtag/domain/user/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package hallym.hashtag.domain.user.service;
-
-import hallym.hashtag.domain.user.dto.UserRequestDto;
-
-import java.util.List;
-
-public interface UserService {
-    String register(UserRequestDto userRequestDto);
-}
--- src/main/java/hallym/hashtag/global/config/SecurityConfig.java
+++ src/main/java/hallym/hashtag/global/config/SecurityConfig.java
@@ -45,10 +45,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
 //                .antMatchers("/user/**")
 //                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
-                .antMatchers("/manager/**")
-                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
-                .antMatchers("/admin/**")
-                .access("hasRole('ROLE_ADMIN')")
+//                .antMatchers("/manager/**")
+//                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+//                .antMatchers("/admin/**")
+//                .access("hasRole('ROLE_ADMIN')")
                 .anyRequest().permitAll();
     }
 }
