90a78be5e030460bdde0c8d485446372f55997d6
mijin0721 <kgmi2010@naver.com>
2023-05-21 20:24:27 +0900

    delete : erd 설계 맞게 다시 디자인하기 위해서 room과 roomres페키지 삭제

--- src/main/java/hallym/hashtag/domain/room/repository/RoomUseTimeRepository.java
+++
@@ -1,9 +0,0 @@
-package hallym.hashtag.domain.room.repository;
-
-import hallym.hashtag.domain.room.entity.RoomUseTime;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoomUseTimeRepository extends JpaRepository<RoomUseTime, Long> {
-}
