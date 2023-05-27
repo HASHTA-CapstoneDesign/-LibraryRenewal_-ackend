6a4129c0a6d26252196896ad9924f13301535bcb
mijin0721 <kgmi2010@naver.com>
2023-05-28 00:11:07 +0900

    build : jar 파일 압축

--- src/main/java/hallym/hashtag/global/config/SecurityConfig.java
+++ src/main/java/hallym/hashtag/global/config/SecurityConfig.java
@@ -1,16 +1,9 @@
 package hallym.hashtag.global.config;
 
-import hallym.hashtag.domain.user.service.UserServicelmpl;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +12,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-        .authorizeRequests()
+                .authorizeRequests()
                 .antMatchers("/api/user/**").permitAll()
                 .anyRequest().permitAll();
     }
